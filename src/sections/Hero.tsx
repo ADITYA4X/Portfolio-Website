4be5e6fd59896e7dedaa705caeb880c38e8841e6
aditya <adityakunghadkar@gmail.com>
2025-05-17 07:01:59 +0530

    Portfolio: Update the Hero-section with HeroOrbit animation of portfolio

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,18 +2,53 @@ import devImage from "@/assets/images/soft-dev.png";
 import Image from "next/image";
 import grainImage from "@/assets/images/grain.jpg";
 import StarIcon from "@/assets/icons/star.svg";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
+import CircleIcon from "@/assets/icons/circle.png";
+import { HeroOrbit } from "@/components/HeroOrbit";
 
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-      <div
-        className="absolute inset-0 -z-30 opacity-5 "
-        style={{ backgroundImage: `url(${grainImage.src})` }}
-      ></div>
-      <div className="size-[620px] hero-ring"></div>
-      <div className="size-[820px] hero-ring"></div>
-      <div className="size-[1020px] hero-ring"></div>
-      <div className="size-[1220px] hero-ring"></div>
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+        <div
+          className="absolute inset-0 -z-30 opacity-5 "
+          style={{ backgroundImage: `url(${grainImage.src})` }}
+        ></div>
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[820px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
+        <HeroOrbit size={770} rotation={-72}>
+          <StarIcon className="size-20 text-orange-700" />
+        </HeroOrbit>
+        <HeroOrbit size={550} rotation={20}>
+          <StarIcon className="size-12 text-orange-800" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={98}>
+          <StarIcon className="size-8 text-orange-700" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotation={-15}>
+          <SparkleIcon className="size-8 text-orange-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={79}>
+          <SparkleIcon className="size-5 text-orange-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={178}>
+          <SparkleIcon className="size-10 text-orange-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={710} rotation={144}>
+          <SparkleIcon className="size-10 text-orange-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotation={85}>
+          <div className="size-3 rounded-full bg-orange-300/20"></div>
+        </HeroOrbit>
+        <HeroOrbit size={520} rotation={-41}>
+          <div className="size-2 rounded-full bg-orange-300/20"></div>
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-5}>
+          <div className="size-2 rounded-full bg-orange-300/20"></div>
+        </HeroOrbit>
+      </div>
 
       <div className="container">
         <div className="flex flex-col items-center ">
@@ -34,8 +69,8 @@ export const HeroSection = () => {
             Hello, My name is <span className=" pl-2 ">ADITYA</span>{" "}
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
-            &quot;Grounded in electrons, thriving in algorithms &mdash; I
-            transformed my path from power to programming&quot;
+            &quot;Grounded in electrons, thriving in algorithms - I transformed
+            my path from power to programming&quot;
           </p>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
