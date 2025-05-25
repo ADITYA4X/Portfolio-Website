4e206acfd924b8767afe846a307071f670579eb7
aditya <adityakunghadkar@gmail.com>
2025-05-25 19:27:40 +0530

    Portfolio: Add My-Toolbox, Other-Project and Map-Image Cards

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -4,6 +4,20 @@ import { useState } from "react";
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
 import StarIcon from "@/assets/icons/star.svg";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import TypescriptIcon from "@/assets/icons/typescript.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import NextIcon from "@/assets/icons/nextjs.svg";
+import ExpressIcon from "@/assets/icons/expressjs.svg";
+import MongoDBIcon from "@/assets/icons/mongodb.svg";
+import TailwindIcon from "@/assets/icons/tailwind-css.svg";
+import Github from "@/assets/icons/github.svg";
+import PostmanIcon from "@/assets/icons/postman.svg";
+import { TechIcon } from "@/components/TechIcon";
+import mapImage from "@/assets/images/map.png";
+import Image from "next/image";
+import smileMemoji from "@/assets/images/memoji-smile.png";
+import { to } from "./../../.next/static/chunks/main-app";
 
 const qualificationData = [
   {
@@ -44,6 +58,90 @@ const qualificationData = [
         platform: "Cohort - Harkirat Singh",
       },
     ],
+  },
+];
+
+const toolboxItems = [
+  {
+    title: "JavaScript",
+    iconType: JavascriptIcon,
+  },
+  {
+    title: "TypeScript",
+    iconType: TypescriptIcon,
+  },
+  {
+    title: "React",
+    iconType: ReactIcon,
+  },
+  {
+    title: "Next.js",
+    iconType: NextIcon,
+  },
+
+  {
+    title: "Express.js",
+    iconType: ExpressIcon,
+  },
+  {
+    title: "MongoDB",
+    iconType: MongoDBIcon,
+  },
+  {
+    title: "Tailwind CSS",
+    iconType: TailwindIcon,
+  },
+  {
+    title: "GitHub",
+    iconType: Github,
+  },
+  {
+    title: "Postman",
+    iconType: PostmanIcon,
+  },
+];
+
+const projects = [
+  {
+    title: "Portfolio-Website",
+    emoji: "👨‍💻",
+    link: "https://github.com/ADITYA4X/Portfolio-Website",
+  },
+  {
+    title: "the-medium-blog",
+    emoji: "📑",
+    link: "https://github.com/ADITYA4X/MERN-PROJECT",
+  },
+  {
+    title: "The Natours",
+    emoji: "🌍",
+    link: "https://github.com/ADITYA4X/the-natours-project",
+  },
+  {
+    title: "Fast-React-Pizza-Co",
+    emoji: "🍕",
+    link: "https://example.com/project2",
+  },
+
+  {
+    title: "Omnifood",
+    emoji: "🍽️",
+    link: "https://omnifood-adityak.netlify.app/",
+  },
+  {
+    title: "Forkify",
+    emoji: "🍽️",
+    link: "https://forkify-adityakunghadkar.netlify.app/",
+  },
+  {
+    title: "Drag-Drop",
+    emoji: "📌",
+    link: "https://github.com/ADITYA4X/Drag-Drop",
+  },
+  {
+    title: "worldwise",
+    emoji: "🌍",
+    link: "https://github.com/ADITYA4X/React-Projects/tree/master/worldwise",
   },
 ];
 
@@ -125,6 +223,56 @@ export const AboutSection = () => {
                   );
                 })}
           </div>
+        </Card>
+
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My Toolbox</h3>
+
+            <p>
+              Explore the technologies and tools I use to craft the beautiful
+              application
+            </p>
+          </div>
+          <div>
+            {toolboxItems.map((item) => (
+              <div key={item.title} className="">
+                <TechIcon component={item.iconType} />
+                <span className="text-sm font-medium">{item.title}</span>
+              </div>
+            ))}
+          </div>
+        </Card>
+
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>Other Projects</h3>
+            <p>
+              I have worked on various projects, including a few that are not
+              listed in the Projects section. Here are some of them:
+            </p>
+          </div>
+          <div>
+            {projects.map((project) => (
+              <a
+                key={project.title}
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-2"
+              >
+                <span className="text-lg">{project.emoji}</span>
+                <span>{project.title}</span>
+              </a>
+            ))}
+          </div>
+        </Card>
+
+        <Card>
+          <Image src={mapImage} alt="map" />
+          <Image src={smileMemoji} alt="Smile Memoji" className="" />
         </Card>
       </div>
     </div>
