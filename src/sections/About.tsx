5837c1cf4c3211a81a2e8f5931807df468ff399d
aditya <adityakunghadkar@gmail.com>
2025-05-27 23:41:16 +0530

    Portfolio: Create CardHeader component and add to Academic Cards

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -17,7 +17,7 @@ import { TechIcon } from "@/components/TechIcon";
 import mapImage from "@/assets/images/map.png";
 import Image from "next/image";
 import smileMemoji from "@/assets/images/memoji-smile.png";
-import { to } from "./../../.next/static/chunks/main-app";
+import { CardHeader } from "@/components/CardHeader";
 
 const qualificationData = [
   {
@@ -38,20 +38,24 @@ const qualificationData = [
     title: "Certification",
     data: [
       {
-        course: "Algorithms on Strings and Algorithmic Toolbox",
-        platform: "Coursera",
+        course: "Algorithms on Strings",
+        platform: "University of California SanDiego - Coursera(2023)",
+      },
+      {
+        course: "Algorithmic Toolbox",
+        platform: "University of California SanDiego-Coursera(2023)",
       },
       {
         course: "The Complete JavaScript Course 2024: From Zero to Expert!",
-        platform: "Udemy",
+        platform: "Jonas Schmedtmann - Udemy",
       },
       {
         course: "The Ultimate React Course 2024: React, Next.js, Redux & More",
-        platform: "Udemy",
+        platform: "Jonas Schmedtmann - Udemy",
       },
       {
         course: "Node.js, Express, MongoDB & More: The Complete Bootcamp",
-        platform: "Udemy",
+        platform: "Jonas Schmedtmann - Udemy",
       },
       {
         course: "0-100 Full Stack Web Development (Advanced Backend+DevOps)",
@@ -146,7 +150,7 @@ const projects = [
 ];
 
 export const AboutSection = () => {
-  const [activeTab, setActiveTab] = useState("Education");
+  const [activeTab, setActiveTab] = useState("Certification");
 
   const getData = (arr: { title: string; data: any[] }[], title: string) => {
     return arr.find((item) => item.title === title);
@@ -155,125 +159,134 @@ export const AboutSection = () => {
   const activeData = getData(qualificationData, activeTab);
 
   return (
-    <div className="pb-96">
-      <SectionHeader
-        eyebrow="About Me"
-        title="A Glimpse Into My World"
-        description="Explore the heart of my work—what I’m passionate about, and the vision that fuels me."
-      />
-      <div>
-        <Card>
-          {/* Tab Triggers */}
-          <div className="flex justify-start">
-            <div className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-              {["Education", "Certification"].map((tab) => (
-                <button
-                  key={tab}
-                  onClick={() => setActiveTab(tab)}
-                  className={`px-4 py-1.5 text-sm rounded-full transition-all duration-300 nav-item ${
-                    activeTab === tab
-                      ? "bg-white text-zinc-900 hover:bg-white/80"
-                      : "text-white/80 hover:text-white"
-                  }`}
-                >
-                  {tab}
-                </button>
+    <div className="py-20">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About Me"
+          title="A Glimpse Into My World"
+          description="Explore the heart of my work—what I’m passionate about, and the vision that fuels me."
+        />
+        <div className="mt-20">
+          <Card className="h-[420px]">
+            <CardHeader
+              title="Academic & Professional Highlights"
+              description="Explore my academic milestones and certifications that shaped my skills and knowledge."
+            />
+            <div className="flex flex-col mt-6">
+              {/* Tab Triggers */}
+              <div className="flex justify-center items-center ">
+                <div className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+                  {["Education", "Certification"].map((tab) => (
+                    <button
+                      key={tab}
+                      onClick={() => setActiveTab(tab)}
+                      className={`px-4 py-1.5 text-sm rounded-full transition-all duration-300 nav-item ${
+                        activeTab === tab
+                          ? "bg-white text-zinc-900 hover:bg-white/80"
+                          : "text-white/80 hover:text-white"
+                      }`}
+                    >
+                      {tab}
+                    </button>
+                  ))}
+                </div>
+              </div>
+
+              {/* Tab Content */}
+
+              <div className="flex flex-col gap-y-2 mt-4 overflow-auto max-h-[200px]">
+                {activeTab === "Education"
+                  ? activeData?.data.map((item, index) => {
+                      const { degree, institution } = item;
+                      return (
+                        <div className="flex gap-x-6 group" key={index}>
+                          <div className="h-[64px] w-[1px] bg-border relative ml-2 mt-2">
+                            <div className="w-[11px] h-[11px] rounded-full bg-orange-600 absolute -left-[5px] md:group-hover:translate-y-[34px] group-hover:translate-y-[64px] transition-all duration-500"></div>
+                          </div>
+                          <div>
+                            <div className="font-semibold text-md mb-2 text-white/80">
+                              {degree},
+                            </div>
+                            <div className="text-sm font-normal mb-4 text-white/60">
+                              {institution}
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })
+                  : activeData?.data.map((item, index) => {
+                      const { course, platform } = item;
+                      return (
+                        <div className="flex gap-x-6 group" key={index}>
+                          <div className="h-[54px] w-[1px] bg-border relative ml-2 mt-2">
+                            <div className="w-[11px] h-[11px] rounded-full bg-orange-600 absolute -left-[5px] md:group-hover:translate-y-[44px] group-hover:translate-y-[54px] transition-all duration-500"></div>
+                          </div>
+                          <div>
+                            <div
+                              className="font-semibold text-md mb-2 text-white/80
+                "
+                            >
+                              {course},
+                            </div>
+                            <div className="text-sm font-medium mb-4 text-white/60">
+                              {platform}
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+              </div>
+            </div>
+          </Card>
+
+          <Card>
+            <div>
+              <StarIcon />
+              <h3>My Toolbox</h3>
+
+              <p>
+                Explore the technologies and tools I use to craft the beautiful
+                application
+              </p>
+            </div>
+            <div>
+              {toolboxItems.map((item) => (
+                <div key={item.title} className="">
+                  <TechIcon component={item.iconType} />
+                  <span className="text-sm font-medium">{item.title}</span>
+                </div>
               ))}
             </div>
-          </div>
+          </Card>
 
-          {/* Tab Content */}
+          <Card>
+            <div>
+              <StarIcon />
+              <h3>Other Projects</h3>
+              <p>
+                I have worked on various projects, including a few that are not
+                listed in the Projects section. Here are some of them:
+              </p>
+            </div>
+            <div>
+              {projects.map((project) => (
+                <a
+                  key={project.title}
+                  href={project.link}
+                  className="flex items-center gap-2"
+                >
+                  <span className="text-lg">{project.emoji}</span>
+                  <span>{project.title}</span>
+                </a>
+              ))}
+            </div>
+          </Card>
 
-          <div className="flex flex-col gap-y-4 mt-8">
-            {activeTab === "Education"
-              ? activeData?.data.map((item, index) => {
-                  const { degree, institution } = item;
-                  return (
-                    <div className="flex gap-x-8 group" key={index}>
-                      <div className="h-[84px] w-[1px] bg-border relative ml-2">
-                        <div className="w-[11px] h-[11px] rounded-full bg-orange-600 absolute -left-[5px] md:group-hover:translate-y-[64px] group-hover:translate-y-[84px] transition-all duration-500"></div>
-                      </div>
-                      <div>
-                        <div className="font-semibold text-md mb-2">
-                          {degree},
-                        </div>
-                        <div className="text-sm font-normal mb-4">
-                          {institution}
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })
-              : activeData?.data.map((item, index) => {
-                  const { course, platform } = item;
-                  return (
-                    <div className="flex gap-x-8 group" key={index}>
-                      <div className="h-[84px] w-[1px] bg-border relative ml-2">
-                        <div className="w-[11px] h-[11px] rounded-full bg-orange-600 absolute -left-[5px] md:group-hover:translate-y-[64px] group-hover:translate-y-[84px] transition-all duration-500"></div>
-                      </div>
-                      <div>
-                        <div className="font-semibold text-md mb-2">
-                          {course},
-                        </div>
-                        <div className="text-sm font-medium mb-4">
-                          {platform}
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
-          </div>
-        </Card>
-
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>My Toolbox</h3>
-
-            <p>
-              Explore the technologies and tools I use to craft the beautiful
-              application
-            </p>
-          </div>
-          <div>
-            {toolboxItems.map((item) => (
-              <div key={item.title} className="">
-                <TechIcon component={item.iconType} />
-                <span className="text-sm font-medium">{item.title}</span>
-              </div>
-            ))}
-          </div>
-        </Card>
-
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>Other Projects</h3>
-            <p>
-              I have worked on various projects, including a few that are not
-              listed in the Projects section. Here are some of them:
-            </p>
-          </div>
-          <div>
-            {projects.map((project) => (
-              <a
-                key={project.title}
-                href={project.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center gap-2"
-              >
-                <span className="text-lg">{project.emoji}</span>
-                <span>{project.title}</span>
-              </a>
-            ))}
-          </div>
-        </Card>
-
-        <Card>
-          <Image src={mapImage} alt="map" />
-          <Image src={smileMemoji} alt="Smile Memoji" className="" />
-        </Card>
+          <Card>
+            <Image src={mapImage} alt="map" />
+            <Image src={smileMemoji} alt="Smile Memoji" className="" />
+          </Card>
+        </div>
       </div>
     </div>
   );
