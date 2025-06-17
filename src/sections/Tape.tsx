3efaf8f829d77a7aa4226ccce2a24d7eed1b484d
aditya <adityakunghadkar@gmail.com>
2025-06-17 23:56:57 +0530

    Portfolio: Update the Move-Left animation in Portfolio Tape Section

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,4 +1,5 @@
 import StarIcon from "@/assets/icons/star.svg";
+import { Fragment } from "react";
 
 const words = [
   "Dream ",
@@ -18,16 +19,22 @@ export const TapeSection = () => {
   return (
     <div className="py-16 lg:py-24 overflow-x-clip">
       <div className=" -mx-1 ">
-        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]  uppercase font-semibold text-xl">
-          <div className="flex flex-none">
-            {words.map((word) => (
-              <div key={word} className="inline-flex items-center ">
-                <span className="bg-gradient-to-r from-stone-300 to-stone-600  text-center bg-clip-text text-transparent px-8 ">
-                  {word}
-                </span>
-                <StarIcon className="size-3 text-white/50" />
-              </div>
-            ))}
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] uppercase font-semibold text-xl">
+          <div className="flex flex-none animate-move-left [animation-duration:30s]">
+            {[
+              ...new Array(2).fill(0).map((_, idx) => (
+                <Fragment key={idx}>
+                  {words.map((word) => (
+                    <div key={word} className="inline-flex items-center ">
+                      <span className="bg-gradient-to-r from-yellow-300 to-red-600  text-center bg-clip-text text-transparent px-8 ">
+                        {word}
+                      </span>
+                      <StarIcon className="size-3 text-white/50" />
+                    </div>
+                  ))}
+                </Fragment>
+              )),
+            ]}
           </div>
         </div>
       </div>
