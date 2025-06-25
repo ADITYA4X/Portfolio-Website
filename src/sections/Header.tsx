9ac59a3d5ebccd58c0fb55d0d27e36899c13bc60
aditya <adityakunghadkar@gmail.com>
2025-06-25 23:42:46 +0530

    Portfolio: Update Header section with Logo and Static Light/Dark Button

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const sections = ["home", "projects", "about", "contact"];
 
@@ -42,7 +43,14 @@ export const Header = () => {
   }, []);
 
   return (
-    <div className="flex justify-center items-center fixed top-3 w-full z-10">
+    <div className="flex justify-between items-center fixed top-5 w-full z-20">
+      <Link href="/" className="mt-[-2px] pl-3">
+        <h1 className="hidden md:block text-3xl md:text-3xl font-medium text-white/80 hover:text-white transition leading-none ">
+          ADITYA{" "}
+          <span className="text-orange-600 text-4xl leading-none">.</span>
+        </h1>
+      </Link>
+
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         {sections.map((section) => (
           <a
@@ -58,6 +66,10 @@ export const Header = () => {
           </a>
         ))}
       </nav>
+      <button className="flex items-center gap-2 px-4 py-1.5 mt-[-2px] mr-5 rounded-full bg-gradient-to-r from-yellow-400/10 to-orange-400/5 text-white/90 text-sm font-medium backdrop-blur hover:from-yellow-400/30 hover:to-orange-400/20 transition-all">
+        <span className="text-base">☀️</span>
+        <span className="hidden md:block">Light</span>
+      </button>
     </div>
   );
 };
